882d9679b0472d3c384483b1303d344affdf04d5
Raymondd <rcmartin@smu.edu>
2013-10-06 23:01:40 -0500

    Added scroll boxes to customizer screen and some cURL code to fetch data from php.api when the databse is up

--- customizer.php
+++ customizer.php
@@ -14,30 +14,41 @@
 	<div id="favorites">
 	<h2>Favorites</h2>
 	<?
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "website");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($ch, CURLOPT_HEADER, FALSE);
-		$response = curl_exec($ch);
-		curl_close($ch);
+		#$ch = curl_init();
+		#curl_setopt($ch, CURLOPT_URL, "web/loadData.php");
+		#curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		#curl_setopt($ch, CURLOPT_HEADER, FALSE);
+		#$response = curl_exec($ch);
+		#curl_close($ch);
 	?>
 	</div>
 
 	<div id="flavors">
 	<h2>Flavors</h2>
-	<? ?>
+	<div class="scroll">
+	This is a scroll box.	
+	</div>
 	</div>
 
-	<div id="fillings">
+	<divid="fillings">
 	<h2>Fillings</h2>
+	<div class="scroll">
+	This is a scrolls box.
+	</div>
 	</div>
 	
 	<div id="icings">
-	<h2>Icing</h2>
+	<h2>Icings</h2>
+	<div class="scroll">
+	This is a scroll box.
+	</div>
 	</div>
 
 	<div id="toppings">
-
+	<h2>Toppings</h2>
+	<div id="buttons">
+		This is the button box.
+	</div>
 	</div>
 
 
